0470e4ff2a22dd4309c75f551c06eb8aad3899cd
MadaDenSnygge <adam.filip.samuelsson@gmail.com>
2026-09-18 15:32:36 +0200

    ändrade kommentarer till engalska

--- hallo.py
+++ hallo.py
@@ -7,26 +7,26 @@
 #print(f'{sorted_dict[i][0]}{" "*amount_of_spaces}{sorted_dict[i][1]}')
 
 def topmost2(dict,n):
-    sorted_dict = sorted(dict.items(),key=lambda item:item[1],reverse=True) #gör om dicten till en lista med tuples och sortar den
-    for i in range(n): #väljer så top 20 ord är med t.ex
-        print(f"{sorted_dict[i][0]:<20}{sorted_dict[i][1]:>20}") #printar det med 20 spaces emmelan så det ser cleant ut
+    sorted_dict = sorted(dict.items(),key=lambda item:item[1],reverse=True) #converts the dict to a list of tuples and sorts it
+    for i in range(n): #makes it so only the top 20 words for example is printed
+        print(f"{sorted_dict[i][0]:<20}{sorted_dict[i][1]:>20}") #prints the word with the correct amount of spaces so it looks clean
 topmost2(dict,2)  
 
 list = ["a","a","b","b","a","the"]
 
 f = open("lab1/eng_stopwords.txt")
 
-#delar upp stopwords i en lista
+#splits the stopword file into a list
 stopwords =[]
 for line in f.readlines():
             stopwords += line.split()
 def word_count(list):
     dict = {}
     for word in list:
-        if word in dict.keys(): #kollar om ordet redan finns med
-            dict[word] +=1 #plussar på antal
-        elif word not in stopwords: # om det inte finns
-            dict[word] = 1 #lägg till
+        if word in dict.keys(): #checks if the word exists in the dictionary
+            dict[word] +=1 #adds 1 to the amount
+        elif word not in stopwords: #if it is new adds it with amount = 1
+            dict[word] = 1 
             
     print(dict)
 
