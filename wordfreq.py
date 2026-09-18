b42dfa622baaadbd537c21838d1a26656af27247
FiggleBottomFB <elias.moll38@gmail.com>
2026-09-18 15:12:10 +0200

    added new word count function and topmost

--- wordfreq.py
+++ wordfreq.py
@@ -33,8 +33,32 @@ def tokenize(testfile):
                 words[words.index(word)] = word[loops:]
 
     words += temp_words
-    print(words)
+    return words
+    # print(words)
+
+f = open("lab1/eng_stopwords.txt")
+stopwords =[]
+for line in f.readlines():
+            stopwords += line.split()
+def word_count(list):
+    dict = {}
+    for word in list:
+        if word in dict.keys():
+            dict[word] +=1
+        elif word not in stopwords:
+            dict[word] = 1
+            
+    # print(dict)
+    return dict
 
 
+def topmost2(dict,n):
+    sorted_dict = sorted(dict.items(),key=lambda item:item[1],reverse=True) 
+    for i in range(n):
+        #len1 = len(str(sorted_dict[i][0]))
+        #len2 = len(str(sorted_dict[i][1]))
+        #amount_of_spaces = 30-len1-len2
+        #print(f'{sorted_dict[i][0]}{" "*amount_of_spaces}{sorted_dict[i][1]}')
+        print(f"{sorted_dict[i][0]:<20}{sorted_dict[i][1]:>20}")
 
-tokenize(testfile)
+topmost2(word_count(tokenize(testfile)), 20)
