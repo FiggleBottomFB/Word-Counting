1194c52de7fd8b212720a99fbbcd2bf73a2bc5da
Vincent O Blomgren <vinblo0723@gmail.com>
2026-09-18 14:29:25 +0200

    topmost function done hopefully

--- topmost.py
+++ topmost.py
@@ -1,7 +1,4 @@
-test_dict = {"apple":3, "banana":7, "pear":2}
 def topmost(dict, n):
-    sorted_dict = sorted(dict.items(),key=lambda item:item[1],reverse=True)
+    sorted_list = sorted(dict.items(),key=lambda item:item[1],reverse=True)
     for i in range(n):
-        pass
-    print(sorted_dict) 
-topmost(test_dict)
+        print(f"{sorted_list[i][0]:<20} {sorted_list[i][1]:>20}")
